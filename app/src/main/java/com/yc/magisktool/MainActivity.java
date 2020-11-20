ef748dbcbb28295ddfdb2a9783bf750d645ac0ed
杨充 <yangchong@doushen.com>
2020-11-20 15:19:28 +0800

    提交demo

--- app/src/main/java/com/yc/magisktool/MainActivity.java
+++ app/src/main/java/com/yc/magisktool/MainActivity.java
@@ -2,13 +2,36 @@ package com.yc.magisktool;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mTv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initFindViewById();
+        initListener();
+    }
+
+    private void initFindViewById() {
+        mTv1 = findViewById(R.id.tv_1);
+    }
+
+    private void initListener() {
+        mTv1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.tv_1:
+                break;
+        }
     }
 }
